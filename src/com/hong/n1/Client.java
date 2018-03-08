bffa105bcb0082e4f1827c750e44c9f0aad63002
jazzhong1 <jazzhong1@naver.com>
2018-03-08 22:02:32 +0900

    result

--- src/com/hong/n1/Client.java
+++ src/com/hong/n1/Client.java
@@ -1,55 +1,42 @@
 package com.hong.n1;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
-	// 서버로 접속하기위한 클라이언트
+	
 	public static void main(String[] args) {
-
-		Socket socket = null;
-
+		Socket socket=null;
+		OutputStream os=null;
+		OutputStreamWriter ow=null;
+		BufferedWriter bw=null;
+		Scanner sc=new Scanner(System.in);
 		try {
-			socket = new Socket("192.168.20.58", 8282);
-
-			boolean check = true;
-			while (check) {
-				OutputStream os = socket.getOutputStream(); // byte를 처리하는애
-				OutputStreamWriter ow = new OutputStreamWriter(os); // 문자를 쓰려고
-				BufferedWriter bw = new BufferedWriter(ow); // 문자열을 쓰려고
-				System.out.println("서버로보낼 메세지를 입력하세요");
-				Scanner sc = new Scanner(System.in);
-				String str = sc.next();
-				bw.write(str + "\r\n");
-				bw.flush();
-
-				if (str.toUpperCase().equals("EXIT")) {
-					break;
-				}
-
-				InputStream is = socket.getInputStream();
-				InputStreamReader ir = new InputStreamReader(is);
-				BufferedReader br = new BufferedReader(ir);
-				String str1 = br.readLine();
-				System.out.println(str1);
-
-				if (str1.toUpperCase().equals("EXIT")) {
-					break;
-				}
-
-			}
-			System.out.println("종료");
+			socket=new Socket("192.168.0.12", 8282);
+			os=socket.getOutputStream();
+			ow=new OutputStreamWriter(os);
+			bw=new BufferedWriter(ow);
+			System.out.println("서버로보낼메시지");
+			String str=sc.next();
+			bw.write(str);
+			bw.flush();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
+				bw.close();
+				ow.close();
+				os.close();
 				socket.close();
 			} catch (Exception e2) {
-				// TODO: handle exception
+				e2.printStackTrace();
 			}
-		} // 서버ip주소,포트
-		// 지금은 자기가 server가 되고 clinet가 된다.
-
+		}
 	}
 }
--- src/com/hong/n1/Server.java
+++ src/com/hong/n1/Server.java
@@ -1,56 +1,43 @@
 package com.hong.n1;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-	//server를 두번돌리면 이미실행되고 있다는 오류를 보낸다.
 	public static void main(String[] args) {
-		ServerSocket ss=null;		//serverSocket 만듬
-		Socket socket;
+		ServerSocket serverSocket=null;
+		Socket socket=null;
+		InputStream is=null;
+		InputStreamReader ir=null;
+		BufferedReader br=null;
 		try {
-			ss=new ServerSocket(8282);	//보안상의 이유로 port는 닫혀있다.	//서버소켓을 만들어준다.
-			System.out.println("클라이언트의 요청을 기다리는 중");
-			socket=ss.accept();	//서버소켓이 받아들이면 	리턴은 soket임 중요 	소켓이 받아 들이면 	//연결 
+			System.out.println("클라이언트와 연결중...");
+			serverSocket=new ServerSocket(8282);
+			socket=serverSocket.accept();
 			
-			boolean check=true;
-			while(check){
+			is=socket.getInputStream();
+			ir=new InputStreamReader(is);
+			br=new BufferedReader(ir);
 			
-			InputStream is=socket.getInputStream(); 
-			InputStreamReader ir=new InputStreamReader(is);
-			BufferedReader br=new BufferedReader(ir);
 			String str=br.readLine();
-			System.out.println(str);
-			
-			if(str.toUpperCase().equals("EXIT")){
-				break;
-			}
-			
-			
-			OutputStream os=socket.getOutputStream();
-			OutputStreamWriter ow=new OutputStreamWriter(os);
-			BufferedWriter bw=new BufferedWriter(ow);
-			Scanner sc=new Scanner(System.in);
-			System.out.println("클라이언트로 보낼 메세지를 입력하세요");
-			String str1=sc.next();
-			bw.write(str1+"\r\n");
-			bw.flush();
-			
-			if(str1.toUpperCase().equals("EXIT")){
-				break;
-			}
-			
-			}
-			System.out.println("종료");
+			System.out.print("클라이언트가 보낸메시지:");
+			System.out.print(str);
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
 			try {
-				ss.close();
+				br.close();
+				ir.close();
+				is.close();
 			} catch (Exception e2) {
 				e2.printStackTrace();
 			}
 		}
 	}
+
 }
