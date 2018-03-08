8fc39734e6192a437f816fdd5f6a3c104f599661
jazzhong1 <jazzhong1@naver.com>
2018-03-08 22:03:38 +0900

    result

--- src/com/hong/n1/Client.java
+++ src/com/hong/n1/Client.java
@@ -1,7 +1,10 @@
 package com.hong.n1;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
@@ -10,33 +13,48 @@ import java.util.Scanner;
 
 public class Client {
 	
-	public static void main(String[] args) {
-		Socket socket=null;
-		OutputStream os=null;
-		OutputStreamWriter ow=null;
-		BufferedWriter bw=null;
-		Scanner sc=new Scanner(System.in);
-		try {
-			socket=new Socket("192.168.0.12", 8282);
-			os=socket.getOutputStream();
-			ow=new OutputStreamWriter(os);
-			bw=new BufferedWriter(ow);
-			System.out.println("서버로보낼메시지");
-			String str=sc.next();
-			bw.write(str);
+	public static void main(String[] args) {Socket socket = null;
+
+	try {
+		socket = new Socket("192.168.20.58", 8282);
+
+		boolean check = true;
+		while (check) {
+			OutputStream os = socket.getOutputStream(); // byte를 처리하는애
+			OutputStreamWriter ow = new OutputStreamWriter(os); // 문자를 쓰려고
+			BufferedWriter bw = new BufferedWriter(ow); // 문자열을 쓰려고
+			System.out.println("서버로보낼 메세지를 입력하세요");
+			Scanner sc = new Scanner(System.in);
+			String str = sc.next();
+			bw.write(str + "\r\n");
 			bw.flush();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				bw.close();
-				ow.close();
-				os.close();
-				socket.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
+
+			if (str.toUpperCase().equals("EXIT")) {
+				break;
 			}
+
+			InputStream is = socket.getInputStream();
+			InputStreamReader ir = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(ir);
+			String str1 = br.readLine();
+			System.out.println(str1);
+
+			if (str1.toUpperCase().equals("EXIT")) {
+				break;
+			}
+
 		}
-	}
+		System.out.println("종료");
+	} catch (Exception e) {
+		e.printStackTrace();
+	} finally {
+		try {
+			socket.close();
+		} catch (Exception e2) {
+			// TODO: handle exception
+		}
+	} // 서버ip주소,포트
+	// 지금은 자기가 server가 되고 clinet가 된다.
+
+}
 }
